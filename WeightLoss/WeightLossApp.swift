51577286562cc087271476c263baaf53c006ce4f
zeta1399 <84230871+zeta1399@users.noreply.github.com>
2023-01-23 02:33:36 +0300

    Initial Commit

---
+++ WeightLoss/WeightLossApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightLossApp.swift
+//  WeightLoss
+//
+//  Created by Enver Enes Keskin on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeightLossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
